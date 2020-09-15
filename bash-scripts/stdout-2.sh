a48e774aec5339f9237f3d9ce74ee6ce6ea4aee4
Edison Garcia Chinas <edisga@microsoft.com>
2020-09-15 18:10:38 -0500

    Adding batch scripts

---
+++ bash-scripts/stdout-2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+log=text.txt
+
+print(){
+    echo "output0" 1>&2
+    echo "output1" 1>&2
+    echo "output2"
+    echo "output3" 1>&2
+}
+
+date >> $log
+echo "Generating output... $(print)" >> $log
+
+sudo apt install htop >> $log
