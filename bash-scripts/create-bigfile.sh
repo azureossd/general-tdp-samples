26d9914474901c724b23d1c9599292f97678821e
Edison Garcia Chinas <edisga@microsoft.com>
2020-09-15 19:37:08 -0500

    Adding batch scripts

--- bash-scripts/create-bigfile.sh
+++ bash-scripts/create-bigfile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 counter=1
-while [ $counter -le 100 ]
+while [ $counter -le 100000 ]
 
 do
-    fallocate -l 100M test.img > /dev/null
+    fallocate -l 1G test.img > /dev/null
     echo "Ping ($counter)"
     counter=$(( $counter + 1 ))
 done
